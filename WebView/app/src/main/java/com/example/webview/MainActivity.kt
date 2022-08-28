305112a37b6735f425b31fa53afc9a7b4dde3188
Brian Veitch <beveitch@gmail.com>
2022-08-27 22:41:54 -0500

    completed recyclerView and WebView

---
+++ WebView/app/src/main/java/com/example/webview/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.webview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var webview: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        title = "Brian's Website"
+        webview = findViewById(R.id.webview)
+        webviewSetup()
+    }
+
+    private fun webviewSetup() {
+        webview.webViewClient = WebViewClient()
+
+        webview.apply {
+            loadUrl("http://www.brianveitch.com")
+            settings.javaScriptEnabled = true
+
+        }
+    }
+
+    override fun onBackPressed() {
+        if (webview.canGoBack()) {
+            webview.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
