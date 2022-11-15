0af4b6e789bab05b9ae1762e18a6a6d17436b7cb
Brian Veitch <beveitch@gmail.com>
2022-11-15 15:19:53 -0600

    updated hints

--- Mad105Project_Sprint1/app/src/main/java/com/example/mad105project_sprint1/operations/Hint.kt
+++ Mad105Project_Sprint1/app/src/main/java/com/example/mad105project_sprint1/operations/Hint.kt
@@ -1,12 +1,31 @@
 package com.example.mad105project_sprint1.operations
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mad105project_sprint1.R
 
 class Hint : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val sp: SharedPreferences = getSharedPreferences("MySharedPreferences", MODE_PRIVATE)
+        val theme = conversion(sp.getInt("theme", R.style.Theme_0))
+        setTheme(theme)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hint)
+    }
+
+    private fun conversion(theme: Int): Int {
+        return when (theme) {
+            R.style.Theme_0 -> R.style.Theme_0_AppCompat_Translucent
+            R.style.Theme_1 -> R.style.Theme_1_AppCompat_Translucent
+            R.style.theme_3 -> R.style.Theme_3_AppCompat_Translucent
+            R.style.theme_4 -> R.style.Theme_4_AppCompat_Translucent
+            R.style.theme_5 -> R.style.Theme_5_AppCompat_Translucent
+            R.style.theme_6 -> R.style.Theme_6_AppCompat_Translucent
+            R.style.theme_7 -> R.style.Theme_7_AppCompat_Translucent
+            else -> R.style.Theme_0_AppCompat_Translucent
+        }
     }
 }
